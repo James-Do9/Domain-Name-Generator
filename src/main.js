6925c721026a97e41d85750f85cd74e9f25baf09
James-Do9 <dojames3@yahoo.com>
2020-12-07 16:29:15 +0000

    Intial commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+function myDomain() {
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big", "amazing", "wonderful"];
+  var noun = ["jogger", "raccoon", "dragon", "monkey", "pig", "chicken"];
+  var dot = [".com", ".io", ".org"];
+  for (var i = 0; i < pronoun.length; i++) {
+    for (var j = 0; j < adj.length; j++) {
+      for (var k = 0; k < noun.length; k++) {
+        for (var l = 0; l < dot.length; l++) {
+          document.querySelector(".domain").innerHTML +=
+            "<li>" + pronoun[i] + adj[j] + noun[k] + dot[l] + "</li>";
+        }
+      }
+    }
+  }
+}
+document.querySelector(".btn").addEventListener("click", myDomain);
